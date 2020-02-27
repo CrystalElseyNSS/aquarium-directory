75557ec044fd22f668d364ff5fcda9369a242dc3
Crystal Elsey <crystalelsey@Crystals-MacBook-Pro.local>
2020-02-27 10:14:29 -0600

    created array iteration of fish list

---
+++ scripts/FishDataProvider.js
@@ -0,0 +1,31 @@
+const fishCollection = [
+    {
+        name: "Harold",
+        species: "Beta Fish",
+        location: "Cabo",
+        length: "6 inches",
+        food: ["human tears", "crumbs"]
+    },
+
+    {
+        name: "Maude",
+        species: "Goldfish",
+        location: "California",
+        length: "2 inches",
+        food: ["avocados", "sunflower seeds"]
+    },
+
+    {
+        name: "Betty",
+        species: "Rainbow Fish",
+        location: "Croation Coast",
+        length: "8 inches",
+        food: ["stardust", "dreams"]
+    }
+]
+
+
+
+export const useFish = () => {
+    return fishCollection
+}
--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,10 @@
-import initializeDetailButtonEvents from './dialogues.js'; 
+import initializeDetailButtonEvents from './dialogs.js'; 
+import { useFish } from './FishDataProvider.js';
 
 initializeDetailButtonEvents();
+
+const arrayOfTheFishObjects = useFish();
+
+for (const currentFishObject of arrayOfTheFishObjects) {
+    console.log(currentFishObject);
+}
