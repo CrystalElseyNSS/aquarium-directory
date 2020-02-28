5d411a69554d1ad7b02c0a377984cff99ddb10af
Crystal Elsey <crystalelsey@Crystals-MacBook-Pro.local>
2020-02-28 10:44:21 -0600

    restored functionality of dialog buttons by added class to fishObjects, to pull capitalized names

--- scripts/fish/Fish.js
+++ scripts/fish/Fish.js
@@ -6,9 +6,9 @@ const Fish = (fishObject) => {
                 <img class="fish__image" src="${fishObject.image}" alt="">
             </div>
             <div class="fish__name">${fishObject.name}</div>
-            <button id="button--${fishObject.name}">Details</button>
+            <button id="button--${fishObject.class}">Details</button>
 
-            <dialog class="dialog--fish" id="details--${fishObject.name}">
+            <dialog class="dialog--fish" id="details--${fishObject.class}">
                 <div>Species: ${fishObject.species}</div>
                 <div>Location: ${fishObject.location}</div>
                 <div>Length: ${fishObject.size}</div>
